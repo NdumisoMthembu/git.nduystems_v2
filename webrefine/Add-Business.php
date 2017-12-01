279732905d62ae5214ae6d45b50c87ff1ad2b862
Ndumiso Mthembu <mrnnmthembu@gmail.com>
2017-12-01 11:08:40 +0200

    add

--- webrefine/Add-Business.php
+++ webrefine/Add-Business.php
@@ -13,9 +13,9 @@
 <div class="services-breadcrumb">
 	<div class="agile_inner_breadcrumb">
 	   <ul class="w3_short">
-			<li><a href="index.html">Home</a><i>||</i></li>
+			<li><a href="Home">Home</a><i>||</i></li>
 			<li>Featured business</li>
-        <li><a href="#Add-Business">Add Your Business</a><i>||</i></li>
+        <li><a href="#Add-Business">Add Your Business</a><i></i></li>
 		</ul>
 	</div>
 </div>
